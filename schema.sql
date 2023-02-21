6a24c58ad41fbc80a48a254dca827e279f415e81
Diego Cuenca <web3.0develope@gmail.com>
2023-02-20 19:47:03 -0500

    Revert "Create animals table"

--- schema.sql
+++ schema.sql
@@ -1,14 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE DATABASE vet_clinic;
-\c vet_clinic
-
 CREATE TABLE animals (
-  id              INT GENERATED ALWAYS AS IDENTITY,
-  pet_name        VARCHAR(15),
-  date_of_birth   DATE,
-  escape_attempts INT,
-  neutered        BOOLEAN,
-  weight_kg       DECIMAL,  
-  PRIMARY KEY(id)
+    name varchar(100)
 );
