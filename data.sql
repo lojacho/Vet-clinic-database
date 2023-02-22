134e8739bc2b555d656c701795cf3f5c48f82d5e
diego <dgcuenca@gmail.com>
2023-02-22 11:05:59 -0500

    Add data

--- data.sql
+++ data.sql
@@ -7,3 +7,17 @@ INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_
 VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', '2017-05-14', 5, true, 11);
+INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', '2020-02-08', 0, FALSE, -11);
+INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Plantmon', '2021-11-15', 2, TRUE, -5.7);
+INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Squirtle', '1993-04-02', 3, FALSE, -12.13);
+INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Angemon', '2005-06-12', 1, TRUE, -45);
+INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Boarmon', '2005-06-07', 7, TRUE, 20.4);
+INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Blossom', '1998-10-13', 3, TRUE, 17);
+INSERT INTO animals (pet_name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Ditto', '2022-05-14', 4, TRUE, 22);
