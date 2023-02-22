ad9e85f497bda87bf8ffad29649c1cccf39b58fc
diego <dgcuenca@gmail.com>
2023-02-22 11:42:40 -0500

    Create a savepoint

--- queries.sql
+++ queries.sql
@@ -49,3 +49,11 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
