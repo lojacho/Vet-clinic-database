7ce895203acd56d3f808366662eb1acc6dac5021
diego <dgcuenca@gmail.com>
2023-02-22 21:01:30 -0500

    Add foreign keys

--- schema.sql
+++ schema.sql
@@ -25,3 +25,12 @@ CREATE TABLE species (
   id SERIAL PRIMARY KEY,
   name TEXT
 );
+
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add the 'species_id' column as a foreign key referencing the 'species' table
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+-- Add the 'owner_id' column as a foreign key referencing the 'owners' table
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
