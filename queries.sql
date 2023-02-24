e6f8266fc982a61aa1577f98c21b2a23e0f2299a
diego <dgcuenca@gmail.com>
2023-02-24 13:47:01 -0500

    Add several queries

--- queries.sql
+++ queries.sql
@@ -111,3 +111,67 @@ LEFT JOIN animals ON owners.id = animals.owner_id
 GROUP BY owners.id
 ORDER BY num_animals DESC
 LIMIT 1;
+
+SELECT animals.pet_name AS animal_name, visits.visit_date
+FROM visits
+JOIN animals ON animals.id = visits.animal_id
+WHERE visits.vet_id = (SELECT id FROM vets WHERE name = 'William Tatcher')
+ORDER BY visits.visit_date DESC
+LIMIT 1;
+
+SELECT COUNT(DISTINCT animal_id) AS num_animals
+FROM visits
+WHERE vet_id = (SELECT id FROM vets WHERE name = 'Stephanie Mendez');
+
+SELECT v.name AS vet_name, s.name AS specialization_name
+FROM vets v
+LEFT JOIN specializations sp ON v.id = sp.vet_id
+LEFT JOIN species s ON sp.species_id = s.id;
+
+SELECT a.pet_name, v.visit_date
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets vt ON v.vet_id = vt.id
+WHERE vt.name = 'Stephanie Mendez' AND v.visit_date BETWEEN '2020-04-01' AND '2020-08-30';
+
+SELECT a.pet_name, COUNT(*) AS num_visits
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+GROUP BY a.id
+ORDER BY num_visits DESC
+LIMIT 1;
+
+SELECT v.visit_date, a.pet_name, vt.name AS vet_name
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets vt ON v.vet_id = vt.id
+WHERE a.owner_id = (SELECT id FROM owners WHERE full_name = 'Maisy Smith')
+ORDER BY v.visit_date
+LIMIT 1;
+
+SELECT a.pet_name, v.visit_date, vt.name AS vet_name, vt.date_of_graduation
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets vt ON v.vet_id = vt.id
+WHERE v.visit_date = (SELECT MAX(visit_date) FROM visits) 
+LIMIT 1;
+
+SELECT COUNT(*)
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets vt ON v.vet_id = vt.id
+WHERE NOT EXISTS (
+  SELECT *
+  FROM specializations s
+  WHERE s.vet_id = vt.id AND s.species_id = a.species_id
+);
+
+SELECT s.name AS specialty, COUNT(*) AS visit_count
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN species s ON a.species_id = s.id
+JOIN vets vt ON v.vet_id = vt.id
+WHERE vt.name = 'Maisy Smith'
+GROUP BY s.name
+ORDER BY visit_count DESC
+LIMIT 1;
